83ebd6747d9c8e940277d292013e4efd0cebe8d2
Mike Robards <mikerobards@icloud.com>
2021-05-02 08:19:30 -0400

    finish lesson 7

---
+++ errors-handling/index.js
@@ -0,0 +1,8 @@
+fetch('example.txt')
+	.then( response => response.text() )
+	.then( text => {
+	  const div = document.createElement("div");
+      div.innerHTML = text;
+      document.body.appendChild(div);
+    })
+    .catch(err => console.error(err));
