169bb1f569ace7a0353985a903a32b172353dd38
Mike Robards <mikerobards@icloud.com>
2021-05-02 08:25:17 -0400

    finish lesson 8

---
+++ async-await/index.js
@@ -0,0 +1,14 @@
+async function getData() {
+// const getData = async () => {
+
+    const response = await fetch('people.json');
+    const data = await response.json();
+
+    data.forEach(person => {
+        const div = document.createElement('div');
+        div.innerHTML = person.name;
+        document.body.appendChild(div);
+    })
+}
+
+getData();
