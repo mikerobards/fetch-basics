f7a3d88cb85bc54f0ac3cbbe7a07618108f73615
Mike Robards <mikerobards@icloud.com>
2021-05-02 09:06:58 -0400

    add error handling

---
+++ fetch-blog-POST/index.js
@@ -0,0 +1,36 @@
+document.getElementById('fetchForm').addEventListener('submit', submitPost)
+
+async function submitPost(e) {
+    e.preventDefault()
+
+    let title = document.getElementById('titleInput').value
+    let body = document.getElementById('bodyInput').value
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({ title: title, body: body }),
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    }
+
+    const postPromise = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        options
+    )
+
+    if (postPromise.ok) {
+        const post = await postPromise.json()
+        // console.log(post)
+
+        title = post.title
+        body = post.body
+
+        document.querySelector('.card-title').innerHTML = title
+        document.querySelector('.card-text').innerHTML = body
+
+        document.getElementById('fetchForm').reset();
+    } else {
+        console.error(`Error: ${postPromise.status}`)
+    }
+}
