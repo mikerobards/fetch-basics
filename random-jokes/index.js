1720b947259b90c9b8666b980b65d5c4705e7fa3
Mike Robards <mikerobards@icloud.com>
2021-05-03 15:07:08 -0400

    add getJoke function

---
+++ random-jokes/index.js
@@ -0,0 +1,14 @@
+const setupDiv = document.getElementById('setup')
+const punchlineDiv = document.getElementById('punchline')
+const punchlineBtn = document.getElementById('punchlineBtn')
+const newJokeBtn = document.getElementById('newJokeBtn')
+
+// Setup an async function called getJoke
+// Create a variable called jokePromise that fetches a joke from https://official-joke-api.appspot.com/jokes/programming/random
+// create a variable called joke that consumes the json data
+
+async function getJoke() {
+    const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+    const joke = await jokePromise.json()
+}
+
